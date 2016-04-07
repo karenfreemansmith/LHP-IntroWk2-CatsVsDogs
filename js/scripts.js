dd2a9471da4c87c4d2046d21c483f9882b478893
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-07 11:25:48 -0700

    add space between photos and text, make text larger, add fade to images

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
 $("button#gocats").click(function() {
   $("ul#cats").prepend("<li>Meow, meow!</li>");
   $("ul#dogs").prepend("<li><strong>BARK!! BARK!!!</li>");
+  $(".cats").css("opacity", ".25");
+  $(".dogs").css("opacity", ".99");
 });
 $("button#godogs").click(function() {
   $("ul#dogs").prepend("<li>Bark, bark!</li>");
   $("ul#cats").prepend("<li><strong>MEOW!! MEOW!!!</li>");
+  $(".dogs").css("opacity", ".25");
+  $(".cats").css("opacity", ".99");
 });
 });
